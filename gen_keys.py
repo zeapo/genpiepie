cdecc73a491d9ba40bb520a8ed91576932bd4b2a
knuthy <knuthy@gmail.com>
2014-03-05 03:00:15 +0100

    added the possibility to generate the masterkey password, encrypt it and save it in a b64 encoded file

--- gen_keys.py
+++ gen_keys.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python
 import sys, argparse
-from genpiepie import gen_key
+from genpiepie import gen_key, gen_masterpwd
 
 def main(argv=None):
-    parser = argparse.ArgumentParser(description="Generate RSA keys")
+    parser = argparse.ArgumentParser(description="Generate RSA keys and a masterkey encrypted with the publickey")
 
     parser.add_argument('--output', help='The prefix of private keys', default='key')
     parser.add_argument('--length', help='The length of the key', default=2048)
+    parser.add_argument('--masterkey', help='A randomly generated masterkey, encoded in b64')
 
     args = parser.parse_args()
-    print(args)
 
     gen_key(args.output, args.length)
+
+    if args.masterkey != None:
+        key = gen_masterpwd(public="{}_pub.pem".format(args.output))
+        try:
+            master_key = open(args.masterkey, 'wb')
+
+        except Exception as err:
+            print("There was a problem: {}", err)
+            print("Here is your key anyway : {}".format(key))
+
+        else:
+            master_key.write(key)
+            master_key.write(b'\n')
+            master_key.close()
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
