5dfd2ad56bada3dca97e6d111588d391c8ccd2ba
knuthy <knuthy@gmail.com>
2014-03-05 03:36:31 +0100

    accept masterpwd file and a privatekey to generate password

--- gen_pwd.py
+++ gen_pwd.py
@@ -9,10 +9,17 @@ def main(argv=None):
 
     parser.add_argument('-u', '--user', help='The username', required=True)
     parser.add_argument('-w', '--website', help='The website', required=True)
+    parser.add_argument('-m', '--masterpwd', help='The master password file')
+    parser.add_argument('-p', '--privatekey', help='The private key file used to decrypt the master password')
 
     args = parser.parse_args()
 
-    masterpwd = getpass('Input the master password: ')
+    onfile = False
+
+    if args.masterpwd:
+        onfile = True
+    else: 
+        masterpwd = getpass('Input the master password: ')
 
     if sys.version_info >= (3,0):
         sym1 = str(input('Input the first symbol: '))
